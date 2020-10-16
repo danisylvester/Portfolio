673115d76d4b7c97da1f6b9bf4c41d2873dad4d5
danisylvester <maria.sylvester10@gmail.com>
2020-10-16 10:45:39 -0500

    Fixed all areas that were not fully responsive, including the buttons on the landing page and the contact section. Reduced padding and margins for mobile and tablet view. Increased padding on the projects section for larger screens.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 /*Tooltip for Find Me icons on side nav */
+
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
+  });
+
+  /*Navbar color change when scroll*/
+$(function () {
+  $(document).scroll(function () {
+    var nav = $(".navbar");
+    nav.toggleClass('scrolled', $(this).scrollTop() > nav.height());
+    });
   });
